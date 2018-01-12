3a863238cf30e08974eed69868ca5a7b0df3a894
Freddie Vargus <fvargus@quantopian.com>
2018-01-12 00:19:30 -0500

    MAINT/TST: Remove copy_market_data

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -88,7 +88,6 @@ from zipline.finance.asset_restrictions import (
 )
 from zipline.testing import (
     FakeDataPortal,
-    copy_market_data,
     create_daily_df_for_asset,
     create_data_portal,
     create_data_portal_from_trade_history,
@@ -4931,7 +4930,6 @@ class TestPanelData(WithTradingEnvironment, ZiplineTestCase):
 
         with tmp_dir() as tmpdir:
             root = tmpdir.getpath('example_data/root')
-            copy_market_data(self.MARKET_DATA_DIR, root)
 
             run_algorithm(
                 start=start_dt,
